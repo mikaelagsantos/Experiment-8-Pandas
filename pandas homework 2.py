5b184d1a0b0c2e4fc08d26c6fe57c616a38a95aa
Mikaela G. Santos <57312908+mikaelagsantos@users.noreply.github.com>
2019-11-26 15:34:19 +0800

    Add files via upload

---
+++ pandas homework 2.py
@@ -0,0 +1,38 @@
+
+
+#problem 2.a
+#df = pd.read_csv('cars.csv')
+#df2 = df[df.index % 2 == 1]
+#print ('\n First five rows with odd-numbered columns: \n', df2[:5])
+
+#problem 2.b
+#df = pd.read_csv('cars.csv')
+#df3 = df.loc[df.Model == 'Mazda RX4']
+#print ('\n The row that contains the Model of Mazda RX4: \n', df3)
+
+
+
+
+#problem 2.c
+import pandas as pd
+df = pd.read_csv('cars.csv')
+df4 = df.loc[(df.Model == 'Camaro Z28', ['cyl'])]
+print(df4)
+
+
+
+
+
+
+
+
+#problem 2.d
+
+#print (df.loc[df['Model'] == 'Camaro Z28', ['Model', 'cyl', 'gear']])
+#print (df.loc[df['Model'] == 'Ford Pantera L', ['Model', 'cyl', 'gear']])
+#print (df.loc[df['Model'] == 'Honda Civic', ['Model', 'cyl', 'gear']])
+
+
+
+
+
